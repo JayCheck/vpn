fa5aec26229003291873f89717354276c59e62c4
jclee <arcyze@gmail.com>
2024-03-17 11:00:17 +0900

    Extract json data and interface to file

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,29 @@
-interface Item {
-  imageUri: string;
-  title: string;
-  description: string;
-  pricing: {
-    original: string;
-    discounted: string;
-  };
-  rating: number;
-  url: string;
+import { Pricing, VPNItem } from './vpn-item';
+import data from './assets/data.json';
+
+
+function createListView(data: VPNItem[]): HTMLUListElement {
+  const listView = document.createElement('ul');
+  listView.classList.add('list-view');
+
+  data.forEach(item => {
+    const listItem = createListItem(item);
+    listView.appendChild(listItem);
+  });
+
+  return listView;
 }
 
-function createListItem(item: Item): HTMLLIElement {
+function createListItem(item: VPNItem): HTMLLIElement {
   const listItem = document.createElement('li');
   listItem.classList.add('list-item');
-
-  const itemContent = document.createElement('div');
-  itemContent.classList.add('item-content');
 
   listItem.addEventListener('click', () => {
     window.location.href = item.url;
   });
+
+  const itemContent = document.createElement('div');
+  itemContent.classList.add('item-content');
 
   const image = createImage(item.imageUri, item.title);
   const textContent = createTextContent(item.title, item.description, item.rating);
@@ -28,6 +32,7 @@ function createListItem(item: Item): HTMLLIElement {
   itemContent.appendChild(image);
   itemContent.appendChild(textContent);
   itemContent.appendChild(pricing);
+
   listItem.appendChild(itemContent);
 
   return listItem;
@@ -40,7 +45,8 @@ function createImage(imageUri: string, title: string): HTMLImageElement {
   return image;
 }
 
-function createTextContent(title: string, description: string, rating: number): HTMLElement {
+function createTextContent(title: string, description: string, rating: number)
+  : HTMLElement {
   const textContent = document.createElement('div');
   textContent.classList.add('text-content');
 
@@ -72,10 +78,11 @@ function createRating(rating: number): HTMLDivElement {
   return ratingElement;
 }
 
-function createPricing(pricing: { original: string; discounted: string }): HTMLDivElement {
+function createPricing(pricing: Pricing): HTMLDivElement {
   const originalPrice = parseFloat(pricing.original);
   const discountedPrice = parseFloat(pricing.discounted);
-  const discountPercentage = ((originalPrice - discountedPrice) / originalPrice * 100).toFixed(0);
+  const discountPercentage =
+    ((originalPrice - discountedPrice) / originalPrice * 100).toFixed(0);
 
   const pricingElement = document.createElement('div');
   pricingElement.classList.add('pricing');
@@ -104,53 +111,6 @@ function createPricing(pricing: { original: string; discounted: string }): HTMLD
   return pricingElement;
 }
 
-function createListView(data: Item[]): HTMLUListElement {
-  const listView = document.createElement('ul');
-  listView.classList.add('list-view');
-
-  data.forEach(item => {
-    const listItem = createListItem(item);
-    listView.appendChild(listItem);
-  });
-
-  return listView;
-}
-
-const data: Item[] = [
-  {
-    imageUri: "assets/images/surfshark.png",
-    title: "SurfShark",
-    description: "A single subscription works simultaneously on multiple devices",
-    pricing: {
-      original: "15.95",
-      discounted: "2.29"
-    },
-    rating: 4,
-    url: "https://get.surfshark.net/aff_c?offer_id=926&aff_id=29693"
-  },
-  {
-    imageUri: "assets/images/ipvanish.webp",
-    title: "IPVanish",
-    description: "Fast speeds, verified no-traffic-logs, and apps for all devices",
-    pricing: {
-      original: "12.99",
-      discounted: "2.75"
-    },
-    rating: 4,
-    url: "https://affiliate.ipvanish.com/aff_c?offer_id=1&aff_id=2727"
-  },
-  {
-    imageUri: "assets/images/proton.png",
-    title: "ProtonVPN",
-    description: "High speed Swiss VPN. Thousands of servers 60+ countries.",
-    pricing: {
-      original: "9.99",
-      discounted: "4.99"
-    },
-    rating: 4,
-    url: "https://go.getproton.me/aff_c?offer_id=26&aff_id=7343"
-  }
-];
-
-const listViewElement = createListView(data);
+const vpnListData: VPNItem[] = data;
+const listViewElement = createListView(vpnListData);
 document.body.appendChild(listViewElement);
